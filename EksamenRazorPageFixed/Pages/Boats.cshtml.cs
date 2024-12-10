5694b556e6e0c4d61ba823acbedd512390d33fa5
Andreas E <andreasegelund123@gmail.com>
2024-12-10 12:39:27 +0100

    Added Razorpages for each model and put reference in header

---
+++ EksamenRazorPageFixed/Pages/Boats.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EksamenRazorPageFixed.Pages
+{
+    public class BoatsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
